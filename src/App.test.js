f0baa836f27270c6f7c94d1bfc5bcbb7502a3d7f
jamesang <jamesang717@gmail.com>
2020-04-17 17:42:00 -0400

    fix and added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Clock from './components/Clock';
+import Greeting from './components/Greeting';
+import Votd from './components/Votd';
 
-test('renders learn react link', () => {
+test('renders time', () => {
+  const { getByText } = render(<Clock />);
+  const clockElement = getByText(/\d{2}:\d{2}|\d{1}:\d{2}/i);
+  expect(clockElement).toBeInTheDocument();
+});
+
+test('renders greeting', () => {
+  const { getByText } = render(<Greeting />);
+  const greetingElement = getByText(/Good .*/i);
+  expect(greetingElement).toBeInTheDocument();
+});
+
+test('renders verse of the day', () => {
+  const { getByText } = render(<Votd />);
+  const votdTextElement = getByText(/Loading\.\.\./i);
+  const votdRefElement = getByText(/Bible\.com/i);
+  expect(votdTextElement).toBeInTheDocument();
+  expect(votdRefElement).toBeInTheDocument();
+});
+
+test('renders Youversion link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const youversionLinkElement = getByText(/YouVersion/i);
+  expect(youversionLinkElement).toBeInTheDocument();
+});
+
+test('renders Unsplash link', () => {
+  const { getByText } = render(<App />);
+  const unsplashLinkElement = getByText(/Unsplash/i);
+  expect(unsplashLinkElement).toBeInTheDocument();
+});
+
+test('renders source link', () => {
+  const { getByText } = render(<App />);
+  const sourceLinkElement = getByText(/@jamesang17/i);
+  expect(sourceLinkElement).toBeInTheDocument();
 });
